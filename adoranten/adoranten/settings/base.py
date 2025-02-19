1b3e502db573645175b481d94f129b7918d3e45f
j4lib <julia.beck@lir.gu.se>
2025-02-19 16:59:40 +0100

    Add a standard page and set up API.

--- adoranten/adoranten/settings/base.py
+++ adoranten/adoranten/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "wagtail.images",
     "wagtail.search",
     "wagtail.admin",
+    "wagtail.api.v2",
     "wagtail",
     "modelcluster",
     "taggit",
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- adoranten/journal/models.py
+++ adoranten/journal/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import (
+    FieldPanel,
+)
+from wagtail.models import (
+    Page
+)
 
 # Create your models here.
+class StandardPage(Page):
+    """
+    Standard default page
+    """
+
+    intro = models.CharField(help_text="Short intro text", blank=True, max_length=250)
+    body = RichTextField(help_text="Text to describe the page", blank=True)
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+        FieldPanel("body"),
+    ]
