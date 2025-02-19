2eeec893ccf6467a6d3eac121fdc732e8937ac60
j4lib <julia.beck@lir.gu.se>
2025-02-19 17:23:55 +0100

    Added API fields as example

--- adoranten/journal/models.py
+++ adoranten/journal/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from wagtail.fields import RichTextField
+from wagtail.api import APIField
 from wagtail.admin.panels import (
     FieldPanel,
 )
+from wagtail.fields import RichTextField
 from wagtail.models import (
     Page
 )
@@ -18,4 +19,9 @@ class StandardPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("intro"),
         FieldPanel("body"),
+    ]
+
+    api_fields = [
+        APIField("intro"),
+        APIField("body"),
     ]
