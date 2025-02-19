651f8003e1ed184baba41556e2591b939acfd518
j4lib <julia.beck@lir.gu.se>
2025-02-19 14:39:59 +0100

    Adoranten first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Adoranten
+Repository for Adoranten. Nicer text tbc
+
+## Installation
+
+Create a conda environment based on the environment file and activate it.
+
+```bash
+$ conda env update --file environment.yml
+$ conda activate adoranten
+```
---
+++ adoranten/adoranten/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-qc*61ay)l=#e377k+l5=p)#6-2-g^b0_^kr)zqkd3s=hp^8)fu"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
