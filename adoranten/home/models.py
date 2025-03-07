ec214dc1d002a2bf6d90146bb9abf7b993ff31de
Tristan Bridge <tbridge360@gmail.com>
2025-03-07 10:12:56 +0100

    new home page / journal models

--- adoranten/home/models.py
+++ adoranten/home/models.py
@@ -1,7 +1,22 @@
+# home/models.py
+
 from django.db import models
-
+from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.api import APIField
+from wagtail_headless_preview.models import HeadlessMixin
 
 
-class HomePage(Page):
-    pass
+class HomePage(HeadlessMixin, Page):
+    description = RichTextField(blank=True, help_text="Description of the website.")
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+    ]
+
+    subpage_types = ['journal.IssuePage']
+
+    api_fields = [
+        APIField("description"),
+    ]
--- adoranten/journal/models.py
+++ adoranten/journal/models.py
@@ -1,28 +1,76 @@
+# journal/models.py
+
 from django.db import models
 from wagtail.api import APIField
-from wagtail.admin.panels import (
-    FieldPanel,
-)
+from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
-from wagtail.models import (
-    Page
-)
+from wagtail.models import Page
 from wagtail_headless_preview.models import HeadlessMixin
 
-# Create your models here.
-class StandardPage(HeadlessMixin, Page):
-    """
-    Standard default page
-    """
+class IssuePage(HeadlessMixin, Page):
+    issue_name = models.CharField(max_length=255)
+    issue_year = models.IntegerField()
+    cover_image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+    pdf_file = models.FileField(
+        upload_to='journal/issues/',
+        null=True,
+        blank=True,
+        help_text="Upload the PDF for the entire issue."
+    )
 
-    intro = models.CharField(help_text="Short intro text", blank=True, max_length=250)
-    body = RichTextField(help_text="Text to describe the page", blank=True)
     content_panels = Page.content_panels + [
-        FieldPanel("intro"),
-        FieldPanel("body"),
+        FieldPanel("issue_name"),
+        FieldPanel("issue_year"),
+        FieldPanel("cover_image"),
+        FieldPanel("pdf_file"),
     ]
 
+    # This page may have ArticlePages as children
+    subpage_types = ['ArticlePage']
+
     api_fields = [
-        APIField("intro"),
-        APIField("body"),
+        APIField("issue_name"),
+        APIField("issue_year"),
+        APIField("cover_image"),
+        APIField("pdf_file"),
+    ]
+
+class ArticlePage(HeadlessMixin, Page):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+    article_title = models.CharField(max_length=255)
+    article_description = RichTextField(blank=True)
+    pdf_file = models.FileField(
+        upload_to='journal/articles/',
+        null=True,
+        blank=True,
+        help_text="Upload the PDF for this specific article."
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("image"),
+        FieldPanel("article_title"),
+        FieldPanel("article_description"),
+        FieldPanel("pdf_file"),
+    ]
+
+    parent_page_types = ['IssuePage']  # Must live under an IssuePage
+    subpage_types = []                 # No children
+
+    api_fields = [
+        APIField("image"),
+        APIField("article_title"),
+        APIField("article_description"),
+        APIField("pdf_file"),
     ]
