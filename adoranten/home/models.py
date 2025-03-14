4630ed1df3703b97af5066f804d13ce5245dcc69
Tristan Bridge <tbridge360@gmail.com>
2025-03-14 10:22:15 +0100

    require certain fields, add pages under home page

--- adoranten/home/models.py
+++ adoranten/home/models.py
@@ -7,13 +7,62 @@ from wagtail.fields import RichTextField
 from wagtail.api import APIField
 from wagtail_headless_preview.models import HeadlessMixin
 
-
 class HomePage(HeadlessMixin, Page):
     description = RichTextField(blank=True, help_text="Description of the website.")
 
     content_panels = Page.content_panels + [
         FieldPanel("description"),
     ]
+
+    subpage_types = [
+        "home.AboutPage",
+        "home.SearchPage",
+        "home.AccessibilityPage",
+    ]
+
+    api_fields = [
+        APIField("description"),
+    ]
+
+class AboutPage(HeadlessMixin, Page):
+    description = RichTextField(blank=True, help_text="About page description.")
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+    ]
+
+    parent_page_types = ["home.HomePage"]
+    subpage_types = []
+
+    api_fields = [
+        APIField("description"),
+    ]
+
+
+class SearchPage(HeadlessMixin, Page):
+    description = RichTextField(blank=True, help_text="Search page description.")
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+    ]
+
+    parent_page_types = ["home.HomePage"]
+    subpage_types = []
+
+    api_fields = [
+        APIField("description"),
+    ]
+
+
+class AccessibilityPage(HeadlessMixin, Page):
+    description = RichTextField(blank=True, help_text="Accessibility page description.")
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+    ]
+
+    parent_page_types = ["home.HomePage"]
+    subpage_types = []
 
     api_fields = [
         APIField("description"),
--- adoranten/journal/models.py
+++ adoranten/journal/models.py
@@ -1,6 +1,7 @@
 # journal/models.py
 
 from django.db import models
+from django.core.exceptions import ValidationError
 from wagtail.api import APIField
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
@@ -9,34 +10,32 @@ from wagtail_headless_preview.models import HeadlessMixin
 
 class Publications(HeadlessMixin, Page):
     parent_page_types = ["wagtailcore.Page"]
-
     subpage_types = ["journal.IssuePage"]
 
 class IssuePage(HeadlessMixin, Page):
-    issue_name = models.CharField(max_length=255)
     issue_year = models.IntegerField()
+    
     cover_image = models.ForeignKey(
         'wagtailimages.Image',
         on_delete=models.SET_NULL,
         null=True,
-        blank=True,
+        blank=False,
         related_name='+'
     )
+
     pdf_file = models.FileField(
         upload_to='journal/issues/',
-        null=True,
-        blank=True,
+        null=False,
+        blank=False,
         help_text="Upload the PDF for the entire issue."
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel("issue_name"),
         FieldPanel("issue_year"),
-        FieldPanel("cover_image"),
+        FieldPanel("cover_image"), 
         FieldPanel("pdf_file"),
     ]
 
-    
     # IssuePage under Publications
     parent_page_types = ["journal.Publications"]
 
@@ -44,32 +43,38 @@ class IssuePage(HeadlessMixin, Page):
     subpage_types = ['ArticlePage']
 
     api_fields = [
-        APIField("issue_name"),
         APIField("issue_year"),
         APIField("cover_image"),
         APIField("pdf_file"),
     ]
+
+    def clean(self):
+        """Ensure cover_image and pdf_file are not empty."""
+        if not self.cover_image:
+            raise ValidationError({'cover_image': "An image is required."})
+        if not self.pdf_file:
+            raise ValidationError({'pdf_file': "A PDF file is required."})
+
 
 class ArticlePage(HeadlessMixin, Page):
     image = models.ForeignKey(
         'wagtailimages.Image',
         on_delete=models.SET_NULL,
         null=True,
-        blank=True,
+        blank=False,
         related_name='+'
     )
-    article_title = models.CharField(max_length=255)
     article_description = RichTextField(blank=True)
+    
     pdf_file = models.FileField(
         upload_to='journal/articles/',
-        null=True,
-        blank=True,
+        null=False,
+        blank=False,
         help_text="Upload the PDF for this specific article."
     )
 
     content_panels = Page.content_panels + [
         FieldPanel("image"),
-        FieldPanel("article_title"),
         FieldPanel("article_description"),
         FieldPanel("pdf_file"),
     ]
@@ -79,7 +84,13 @@ class ArticlePage(HeadlessMixin, Page):
 
     api_fields = [
         APIField("image"),
-        APIField("article_title"),
         APIField("article_description"),
         APIField("pdf_file"),
     ]
+
+    def clean(self):
+        """Ensure image and pdf_file are not empty."""
+        if not self.image:
+            raise ValidationError({'image': "An image is required."})
+        if not self.pdf_file:
+            raise ValidationError({'pdf_file': "A PDF file is required."})
