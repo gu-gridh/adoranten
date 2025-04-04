7e67c2f9b2067edf5b5bdf6d19004919068c152f
j4lib <julia.beck@lir.gu.se>
2025-04-04 15:05:12 +0200

    Streamfield for highlights

--- adoranten/home/models.py
+++ adoranten/home/models.py
@@ -1,17 +1,46 @@
 # home/models.py
-
-from django.db import models
 from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField, StreamField
 from wagtail.api import APIField
+from wagtail import blocks
 from wagtail_headless_preview.models import HeadlessMixin
+
+
+class APIPageChooserBlock(blocks.PageChooserBlock):
+    def get_api_representation(self, value, context=None):
+        if value:
+            return {
+                "id": value.id,
+                "title": value.title
+            }
+
 
 class HomePage(HeadlessMixin, Page):
     description = RichTextField(blank=True, help_text="Description of the website.")
+    latest_issues = StreamField([
+        ("heading", blocks.CharBlock(form_classname="title")),
+        ("issue_list", blocks.ListBlock(
+            (APIPageChooserBlock(page_type="journal.IssuePage")))),
+    ], blank=True,
+       block_counts={
+       "heading": {"max_num": 1},
+       "issue_list": {"max_num": 3},
+    })
+    article_highlights = StreamField([
+        ("heading", blocks.CharBlock(form_classname="title")),
+        ("article_list", blocks.ListBlock(
+            (APIPageChooserBlock(page_type="journal.ArticlePage")))),
+    ], blank=True,
+       block_counts={
+       "heading": {"max_num": 1},
+       "article_list": {"max_num": 3},
+    })
 
     content_panels = Page.content_panels + [
         FieldPanel("description"),
+        FieldPanel("latest_issues"),
+        FieldPanel("article_highlights"),
     ]
 
     subpage_types = [
@@ -23,7 +52,10 @@ class HomePage(HeadlessMixin, Page):
 
     api_fields = [
         APIField("description"),
+        APIField("latest_issues"),
+        APIField("article_highlights"),
     ]
+
 
 class AboutPage(HeadlessMixin, Page):
     description = RichTextField(blank=True, help_text="About page description.")
