ef633b11c31230c135624eddf9ba0c09f4db7cf4
Tristan Bridge <tbridge360@gmail.com>
2025-04-04 15:00:42 +0200

    contact form

--- adoranten/home/models.py
+++ adoranten/home/models.py
@@ -1,11 +1,31 @@
 # home/models.py
 
 from django.db import models
-from wagtail.admin.panels import FieldPanel
-from wagtail.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import (
+    FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    MultiFieldPanel,
+    PublishingPanel,
+)
+from wagtail.models import (
+    DraftStateMixin,
+    PreviewableMixin,
+    RevisionMixin,
+    TranslatableMixin,
+    Page
+)
 from wagtail.fields import RichTextField
 from wagtail.api import APIField
 from wagtail_headless_preview.models import HeadlessMixin
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
+from wagtail.contrib.settings.models import (
+    BaseGenericSetting,
+    register_setting,
+)
+from wagtail.snippets.models import register_snippet
 
 class HomePage(HeadlessMixin, Page):
     description = RichTextField(blank=True, help_text="Description of the website.")
@@ -18,6 +38,7 @@ class HomePage(HeadlessMixin, Page):
         "home.AboutPage",
         "home.SearchPage",
         "home.AccessibilityPage",
+        "home.FormPage",
         "journal.Publications",
     ]
 
@@ -39,7 +60,6 @@ class AboutPage(HeadlessMixin, Page):
         APIField("description"),
     ]
 
-
 class SearchPage(HeadlessMixin, Page):
     description = RichTextField(blank=True, help_text="Search page description.")
 
@@ -54,7 +74,6 @@ class SearchPage(HeadlessMixin, Page):
         APIField("description"),
     ]
 
-
 class AccessibilityPage(HeadlessMixin, Page):
     description = RichTextField(blank=True, help_text="Accessibility page description.")
 
@@ -67,4 +86,25 @@ class AccessibilityPage(HeadlessMixin, Page):
 
     api_fields = [
         APIField("description"),
+    ]
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='form_fields')
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address'),
+                FieldPanel('to_address'),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
     ]
