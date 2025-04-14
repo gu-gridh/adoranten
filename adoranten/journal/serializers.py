caf4572bf6181c1707e9d833b2c6235adf568200
j4lib <julia.beck@lir.gu.se>
2025-04-14 12:16:14 +0200

    Add image to article serializer.

--- adoranten/journal/serializers.py
+++ adoranten/journal/serializers.py
@@ -1,11 +1,14 @@
 from wagtail.api.v2.serializers import PageSerializer
+from wagtail.images.api.fields import ImageRenditionField
 from .models import ArticlePage
 
 
 class ArticlePageSerializer(PageSerializer):
     meta_fields = '__all__'
+    # TODO: Choose thumbnail size instead?
+    image = ImageRenditionField('original')
 
     class Meta:
         model = ArticlePage
-        fields = ['id', 'title', 'pdf_file',
+        fields = ['id', 'title', 'pdf_file', 'image',
                   'author', 'page_range', 'citation', 'tags']
